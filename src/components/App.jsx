c61ed8c1d7ee4dda6f300d001656627d70054a9a
Liudmyla007 <ldatsenko3@gmail.com>
2023-02-18 22:49:17 +0200

    install modern-normalize

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,12 @@
 import css from './App.module.css';
+import { GlobalStyle } from './GlobalStyle';
 
 
 
 export const App = () => {
   return (
     <div className={css.container}>
-   
+     <GlobalStyle/>
       React homework template
     </div>
   );
